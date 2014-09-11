926cddb3189ab12100cbb985b68bf2b2878a541f
Rachel <rachelwilson@wl-214-179.wpa.claremont.edu>
2014-09-11 10:16:03 -0700

    Initial Commit

---
+++ ViewTutorial/RWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWAppDelegate.h
+//  ViewTutorial
+//
+//  Created by Rachel Wilson on 9/11/14.
+//  Copyright (c) 2014 Rachel Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewTutorial
+//
+//  Created by Rachel Wilson on 9/11/14.
+//  Copyright (c) 2014 Rachel Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWAppDelegate class]));
+    }
+}
